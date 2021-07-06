101e338a762be5a9e4a72dfd1c9c953eccdf3440
Daniel Bellfield <dnlbellfield@gmail.com>
2021-07-05 23:07:14 -0700

    settings static

--- spring_water/settings.py
+++ spring_water/settings.py
@@ -125,9 +125,7 @@ USE_TZ = True
 # STATIC_ROOT = BASE_DIR / 'static_collected'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type

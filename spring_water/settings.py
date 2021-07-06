d63e0d572a8923f544fc60ed30eeeceb36baeffc
Daniel Bellfield <dnlbellfield@gmail.com>
2021-07-05 23:05:36 -0700

    settings static

--- spring_water/settings.py
+++ spring_water/settings.py
@@ -121,9 +121,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+
 # STATIC_ROOT = BASE_DIR / 'static_collected'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
@@ -131,4 +135,4 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATIC_ROOT = BASE_DIR / 'static_collected'
+# STATIC_ROOT = BASE_DIR / 'static_collected'
